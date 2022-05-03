a83940a3744255c0a9dd8f1bb7c9fcd22ce6e9d0
Levi.Tracy <Levi.Tracy@IVP-LDD4KXMG3>
2022-05-03 14:35:53 -0400

    Add project files.

---
+++ Inedxing/Program.cs
@@ -0,0 +1,18 @@
+﻿var total  = 0;
+
+for (var index = 0; index <= 25; index++) //have to do all 3 or syntax error
+        total = total + index;
+        {
+    Console.WriteLine($"Index is {total}");
+
+    //Body of for loop
+    }
+//The below, in short form ^^^^
+//int index1 = 0;
+
+
+//while (index1 < 10)
+//    {
+    //Body of loop
+//    index1++;
+//    }
